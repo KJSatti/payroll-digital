52669fa776c8fe785c50970ef900a7256a1c7bfa
Muhammad Khurram Jamil <khurram.jamilsatti@gmail.com>
2025-08-15 00:12:11 +0500

    ---

--- database/migrations/2025_07_12_195127_create_permissionsss_table.php
+++ database/migrations/2025_07_12_195127_create_permissionsss_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('permissions', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
-            $table->text('description')->nullable();
-            $table->timestamps();
-        });
+        // Schema::create('permissions', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name')->unique();
+        //     $table->text('description')->nullable();
+        //     $table->timestamps();
+        // });
     }
 
     /**
