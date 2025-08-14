3a08038c904fbd3b6f1e789e526f1623bbfce9ce
Muhammad Khurram Jamil <khurram.jamilsatti@gmail.com>
2025-08-15 00:59:50 +0500

    ---

--- database/migrations/2025_07_12_195204_create_role_permissions_table.php
+++ database/migrations/2025_07_12_195204_create_role_permissions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('role_permissions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
-            $table->integer('permission_id')->default(0);
+            $table->integer('permission_id')->constrained()->onDelete('cascade');
         });
     }
 
